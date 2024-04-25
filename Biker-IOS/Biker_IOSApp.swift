91b13c8b4d60371eb408b7f57114fa0a722ccbd8
Michael Bamidele <mbamidele29@gmail.com>
2024-04-25 02:23:20 +0100

    Initial Commit

---
+++ Biker-IOS/Biker_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Biker_IOSApp.swift
+//  Biker-IOS
+//
+//  Created by PC on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Biker_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
